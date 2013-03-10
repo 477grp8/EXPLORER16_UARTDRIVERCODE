a35c663471998363691507305f62c7770b6b6571
477grp8 <sriram.ranga.9@gmail.com>
2013-03-10 16:32:49 -0300

    Create SDCARD.h

---
+++ SDCARD.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   SDCARD.h
+ * Author: team8
+ *
+ * Created on March 10, 2013, 3:31 PM
+ */
+
+#ifndef SDCARD_H
+#define  SDCARD_H
+
+#include <p32xxxx.h>
+#include <plib.h>
+
+#endif	/* SDCARD_H */
+
